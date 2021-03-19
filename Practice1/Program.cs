a5a63b3691bcb4ad03f470d501b1f2889f9f19dc
cyliu <cyliu12@gmail.com>
2021-03-19 23:42:22 +0800

    Create Practice1 and install SQLite

---
+++ Practice1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+
+namespace Practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
